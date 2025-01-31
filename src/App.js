9e6ff29125c24de6c411b2fb70524bf094288d0f
LeonieNollevalle <leonie.nollevalle@gmail.com>
2025-01-31 19:02:32 +0100

    lala

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <iframe className="desk" title="desktopcv" width="100%" height="110%" src="https://embed.figma.com/proto/KVpGHddCbtk90ZBTxbjqa4/cv-desktop?page-id=0%3A1&node-id=1-2&viewport=322%2C128%2C0.34&scaling=min-zoom&content-scaling=fixed&embed-host=share" allowfullscreen></iframe>
+      <iframe className="mobile" title="mobilepcv" width="100%" height="120%" src="https://embed.figma.com/proto/eTSEktMSGfWGrUoIJel6Hg/Cv-mobile?page-id=0%3A1&node-id=1-2&viewport=392%2C154%2C0.22&scaling=scale-down&content-scaling=fixed&embed-host=share" allowfullscreen></iframe>    </div>
   );
 }
 
